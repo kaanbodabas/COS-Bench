3f33684edce439f316d82e0d08f7d1ac58427508
Kaan Odabas <ko8584@princeton.edu>
2024-12-29 05:17:32 -0500

    finalize plots, add requirements and readme

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # A Convex Optimization Solver Benchmarking Framework
 
-## Benchmark Results
+## Usage Guide
 
-### Minimum Cost Network Flow LP
+### Installations
+You'll need Python and pip installed first. Then, run `pip install -r requirements.txt` inside your virtual environment.
 
-### Image Deblurring QP
+### Benchmarking
+Just run `python -m scripts.YOUR_SCRIPT_FILENAME_HERE`. Resulting plots go in the `output` folder.
 
-### Robust Portfolio Allocation SOCP
+### Adding a solver
+Using any existing solver as a guide, add a new file in the `solvers` folder and inside it complete the `solve(n, m, P, q, D, b, cones, verbose)` function. Add your solver to `enums.py` and `maps.py`.
 
-### Maxcut Relaxation SDP
+### Adding a problem
+Using any existing problem as a guide, add a new file in the `problems` folder and inside it create a class that inherits from `problem.Instance` and contains functions `__init__`, `solve_original_in_cvxpy`, and `canonicalize`. Add a new testing script in the `scripts` folder, using any existing script as a guide.
+
+### CVXPY
+You can add CVXPY as a solver in `maps.py` to incorporate it in the benchmarks.
+
+### Configuration
+Adjust `NUM_CORES` and `TIME_LIMIT` in `constants.py` as needed.
+
+### Paper
+See the paper for more information on the framework.
--- utils/compute.py
+++ utils/compute.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib import ticker
 import numpy as np
 
 SHIFTED_GEOMETRIC_MEANS = " Shifted Geometric Means"
@@ -7,6 +8,8 @@ FAILURE_RATES = " Failure Rates"
 AVERAGE_SOLVE_TIMES = " Average Solve Time"
 
 plt.rcParams.update({"font.size": 14})
+plt.rcParams.update({"font.family": "Times New Roman"})
+plt.rcParams.update({"figure.figsize": [10, 4]})
 
 def get_solve_times(solver, solutions_df):
     return solutions_df[solutions_df["Solver"] == solver.value]["Solve Time"].to_numpy(dtype=float) + 0.001
@@ -30,7 +33,8 @@ def plot_normalized_geometric_means(solvers, solutions_df, title):
         means[solver] = shifted_geometric_mean(solve_times)
     means = normalized_geometric_mean(means)
 
-    plt.figure(figsize=(12.8, 9.6))
+    plt.figure()
+    plt.ylim(0, 1.1 * max(means.values()))
     bars = plt.bar([key.value for key in means.keys()], means.values())
     for bar in bars:
         height = bar.get_height()
@@ -39,8 +43,9 @@ def plot_normalized_geometric_means(solvers, solutions_df, title):
         else:
             mean = round(height, 2)
         plt.text(bar.get_x() + bar.get_width() / 2, height, str(mean), ha="center", va="bottom")
+    plt.ylabel("Shifted geometric mean")
     plt.title(title + SHIFTED_GEOMETRIC_MEANS)
-    plt.savefig(f"output/{title + SHIFTED_GEOMETRIC_MEANS}")
+    plt.savefig(f"output/{title + SHIFTED_GEOMETRIC_MEANS}", dpi=300, bbox_inches="tight")
     
 def performance_profiles(solvers, solver_times, num_instances, taus, num_taus):
     for p in range(num_instances):
@@ -67,31 +72,32 @@ def plot_performance_profiles(solvers, solutions_df, num_instances, title, num_t
         solver_times[solver] = solve_times
     profiles = performance_profiles(solvers, solver_times, num_instances, taus, num_taus)
 
-    plt.figure(figsize=(12.8, 9.6))
+    plt.figure()
     for solver in solvers:
         plt.plot(taus, profiles[solver], label=solver)
-    plt.xlabel("Performance Ratio")
+    plt.xlabel("Performance ratio")
     plt.xscale("log")
-    plt.ylabel("Ratio of Problems Solved")
+    plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter())
+    plt.ylabel("Ratio of problems solved")
     plt.title(title + PERFORMANCE_PROFILES)
     plt.legend()
-    plt.grid()
-    plt.savefig(f"output/{title + PERFORMANCE_PROFILES}")
+    plt.grid(True, "both")
+    plt.savefig(f"output/{title + PERFORMANCE_PROFILES}", dpi=300, bbox_inches="tight")
 
 def plot_failure_rates(solvers, solutions_df, num_instances, title):
     failure_rates = {}
     for solver in solvers:
         failure_rates[solver] = 100 * get_num_fails(solver, solutions_df) / num_instances
     
-    plt.figure(figsize=(12.8, 9.6))
+    plt.figure()
     plt.ylim(0, 100)
     bars = plt.bar([key.value for key in failure_rates.keys()], failure_rates.values())
     for bar in bars:
         height = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2, height, str(round(height, 4)), ha="center", va="bottom")
-    plt.ylabel("Percent Failed")
+    plt.ylabel("Failure rate [%]")
     plt.title(title + FAILURE_RATES)
-    plt.savefig(f"output/{title + FAILURE_RATES}")
+    plt.savefig(f"output/{title + FAILURE_RATES}", dpi=300, bbox_inches="tight")
 
 def plot_average_solve_times(solvers, solutions_df, num_instances, title):
     averages = {}
@@ -99,11 +105,12 @@ def plot_average_solve_times(solvers, solutions_df, num_instances, title):
         solve_times = get_solve_times(solver, solutions_df)
         averages[solver] = sum(solve_times) / num_instances
 
-    plt.figure(figsize=(12.8, 9.6))
+    plt.figure()
+    plt.ylim(0, 1.1 * max(averages.values()))
     bars = plt.bar([key.value for key in averages.keys()], averages.values())
     for bar in bars:
         height = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2, height, str(round(height, 3)), ha="center", va="bottom")
-    plt.ylabel("Time in Seconds")
+    plt.ylabel("Solve time [sec]")
     plt.title(title + AVERAGE_SOLVE_TIMES)
-    plt.savefig(f"output/{title + AVERAGE_SOLVE_TIMES}")
+    plt.savefig(f"output/{title + AVERAGE_SOLVE_TIMES}", dpi=300, bbox_inches="tight")
